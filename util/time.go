8674767bdbfefede05f731819af6eb9ee966e3d6
Shane <innon.shen@gmail.com>
2023-02-16 16:24:57 +0800

    Update time.go

--- util/time.go
+++ util/time.go
@@ -5,8 +5,6 @@ import (
 	"time"
 )
 
-const DateLayout = "2006-01-02"
-
 // FromToday 计算指定日期距今天的天数差
 // 计算时会舍弃时分秒，只对比日期
 // 如果是过去返回负数，未来返回正数
@@ -15,13 +13,12 @@ func FromToday(layout, value string) (int, error) {
 	if err != nil {
 		return 0, failure.Wrap(err)
 	}
-	targetDate := GetDate(targetTime)
-	todayDate := GetDate(time.Now())
+	targetDate := GetZeroTime(targetTime)
+	todayDate := GetZeroTime(time.Now())
 	return int(targetDate.Sub(todayDate).Hours() / 24), nil
 }
 
-// GetDate 获得时间所属日期
-func GetDate(t time.Time) time.Time {
-	date, _ := time.Parse(DateLayout, t.Format(DateLayout))
-	return date
+// GetZeroTime 获得时间所属日期的 0 点时间
+func GetZeroTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
