5f9c8956f0b0724701b4e6ef5f815d9af6e9d72e
Shane <innon.shen@gmail.com>
2023-02-17 14:33:57 +0800

    update time util

--- util/time.go
+++ util/time.go
@@ -6,36 +6,43 @@ import (
 )
 
 // IsMonthEnd 判断时间是否是所属月的最后一天
-// 警告：必须处理异常，当有异常发生时 bool 的值为 false
-func IsMonthEnd(layout, value string) (bool, error) {
-	t, err := parseTime(layout, value)
-	if err != nil {
-		return false, err
-	}
-	nextDay := t.Add(24 * time.Hour)
-	return t.Month() != nextDay.Month(), nil
+func IsMonthEnd(t time.Time) bool {
+	return t.Month() != t.Add(24*time.Hour).Month()
 }
 
 // FromToday 计算指定日期距今天的天数差
 // 计算时会舍弃时分秒，只对比日期
 // 如果是过去返回负数，未来返回正数
-func FromToday(layout, value string) (int, error) {
-	targetTime, err := parseTime(layout, value)
-	if err != nil {
-		return 0, err
-	}
-	targetDate := getZeroTime(targetTime)
+func FromToday(t time.Time) int {
+	date := getZeroTime(t)
 	todayDate := getZeroTime(time.Now())
-	return int(targetDate.Sub(todayDate).Hours() / 24), nil
+	return int(date.Sub(todayDate).Hours() / 24)
+}
+
+// ParseTimes 批量解析时间
+// 返回正确解析的时间数组、解析失败的字符串将及其错误原因
+func ParseTimes(layout string, value []string) (ts []time.Time, em map[string]error) {
+	for _, v := range value {
+		t, err := ParseTime(layout, v)
+		if err != nil {
+			if em == nil {
+				em = make(map[string]error)
+			}
+			em[v] = err
+		} else {
+			ts = append(ts, t)
+		}
+	}
+	return
+}
+
+// ParseTime 用本地时区解析时间
+func ParseTime(layout, value string) (time.Time, error) {
+	t, err := time.ParseInLocation(layout, value, time.Now().Location())
+	return t, failure.Wrap(err)
 }
 
 // getZeroTime 获得时间所属日期的 0 点时间
 func getZeroTime(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-}
-
-// parseTime 用本地时区解析时间
-func parseTime(layout, value string) (time.Time, error) {
-	t, err := time.ParseInLocation(layout, value, time.Now().Location())
-	return t, failure.Wrap(err)
 }
--- util/time_test.go
+++ util/time_test.go
@@ -5,14 +5,34 @@ import (
 	"testing"
 )
 
+const (
+	layout = "data_date=20060102"
+)
+
+func TestParseTimes(t *testing.T) {
+	values := []string{
+		"data_date=20230130",
+		"data_date=20230131",
+		"data_date=20230222",
+		"data_date=20230230",
+	}
+	ts, em := ParseTimes(layout, values)
+	for _, t := range ts {
+		fmt.Println(t)
+	}
+	for s, err := range em {
+		fmt.Println(s + ": " + err.Error())
+	}
+}
+
 func TestIsMonthEnd(t *testing.T) {
-	res, err := IsMonthEnd("20060102", "20230131")
-	fmt.Println(res)
+	time, err := ParseTime(layout, "data_date=20230130")
 	fmt.Println(fmt.Sprintf("%+v", err))
+	fmt.Println(IsMonthEnd(time))
 }
 
 func TestFromToday(t *testing.T) {
-	days, err := FromToday("20060102", "20230216")
-	fmt.Println(days)
+	time, err := ParseTime(layout, "data_date=20230131")
 	fmt.Println(fmt.Sprintf("%+v", err))
+	fmt.Println(FromToday(time))
 }
