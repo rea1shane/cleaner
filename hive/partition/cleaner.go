a2fef2c52588d4dd1dc28d00c4e28ab048adf337
Shane <innon.shen@gmail.com>
2023-02-22 15:03:52 +0800

    update

--- hive/partition/cleaner.go
+++ hive/partition/cleaner.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/colinmarc/hdfs"
+	"github.com/rea1shane/cleaner/hive/partition/policy/mod"
+	"github.com/rea1shane/cleaner/util"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"strings"
 )
 
 type cleaner struct {
@@ -23,21 +28,77 @@ type cleaner struct {
 	} `yaml:"policy"`
 }
 
+var (
+	c                   *cleaner
+	client              *hdfs.Client
+	savePartitions      = make(map[string]map[string][]string)
+	needCleanPartitions = make(map[string]map[string][]string)
+	wrongTables         []string
+	wrongPartitions     = make(map[string]map[string][]string)
+)
+
 func main() {
 	// TODO 更换 yaml 路径
 	file, err := ioutil.ReadFile("hive/partition/setting.yaml")
 	if err != nil {
 		panic(err)
 	}
-	var c *cleaner
 	err = yaml.Unmarshal(file, &c)
 	if err != nil {
 		panic(err)
 	}
-	for _, t := range c.Policy.Mod1 {
-		// TODO 获取该表的分区数组
-		// TODO 用规则决定删除哪些分区
-		// TODO 删除分区
+
+	client, err = util.GetHdfsClient(c.Hive.Storage.Hdfs.Address)
+	if err != nil {
+		panic(err)
 	}
-	// TODO Mod2 Mod3
+
+	for _, t := range c.Policy.Mod1 {
+		groupHivePartitions(mod.M1, t)
+	}
+	for _, t := range c.Policy.Mod2 {
+		groupHivePartitions(mod.M2, t)
+	}
+	for _, t := range c.Policy.Mod3 {
+		groupHivePartitions(mod.M3, t)
+	}
+
+	fmt.Println(fmt.Sprintf("%+v", savePartitions))
+	fmt.Println()
+	fmt.Println(fmt.Sprintf("%+v", needCleanPartitions))
+	fmt.Println()
+	fmt.Println(fmt.Sprintf("%+v", wrongPartitions))
+
+	// TODO 删除分区
+}
+
+func groupHivePartitions(m mod.Mod, t string) {
+	if !strings.Contains(t, ".") {
+		wrongTables = append(wrongTables, t)
+		return
+	}
+
+	ss := strings.Split(t, ".")
+	hdfsPath := c.Hive.Storage.Hdfs.Path + "/" + ss[0] + "/" + ss[1]
+
+	partitions, _ := util.ListHdfsSubDirs(client, hdfsPath)
+	matched, unmatched, errorPartitions := m.Group(c.Hive.Storage.PartitionLayout, partitions)
+	if len(matched) != 0 {
+		if savePartitions[ss[0]] == nil {
+			savePartitions[ss[0]] = make(map[string][]string)
+		}
+		savePartitions[ss[0]][ss[1]] = matched
+	}
+	if len(unmatched) != 0 {
+		if needCleanPartitions[ss[0]] == nil {
+			needCleanPartitions[ss[0]] = make(map[string][]string)
+		}
+		needCleanPartitions[ss[0]][ss[1]] = unmatched
+	}
+	if len(errorPartitions) != 0 {
+		if wrongPartitions[ss[0]] == nil {
+			wrongPartitions[ss[0]] = make(map[string][]string)
+		}
+		wrongPartitions[ss[0]][ss[1]] = errorPartitions
+	}
 }
--- util/hdfs.go
+++ util/hdfs.go
@@ -1,1 +1,26 @@
 package util
+
+import (
+	"github.com/colinmarc/hdfs"
+	"github.com/morikuni/failure"
+)
+
+func GetHdfsClient(address string) (client *hdfs.Client, err error) {
+	client, err = hdfs.New(address)
+	err = failure.Wrap(err)
+	return
+}
+
+func ListHdfsSubDirs(client *hdfs.Client, path string) (subDirs []string, err error) {
+	infos, err := client.ReadDir(path)
+	if err != nil {
+		err = failure.Wrap(err)
+		return
+	}
+	for _, info := range infos {
+		if info.IsDir() {
+			subDirs = append(subDirs, info.Name())
+		}
+	}
+	return
+}
